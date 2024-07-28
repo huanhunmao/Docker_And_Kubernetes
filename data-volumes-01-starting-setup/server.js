59a255a7f4a7f8e39bb563e5d77c3f3c29e4f7e0
Marxu <fuhaojun@52tt.com>
2024-07-28 09:06:33 +0800

    feat: init

---
+++ data-volumes-01-starting-setup/server.js
@@ -0,0 +1,51 @@
+const fs = require('fs').promises;
+const exists = require('fs').exists;
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static('public'));
+app.use('/feedback', express.static('feedback'));
+
+app.get('/', (req, res) => {
+  const filePath = path.join(__dirname, 'pages', 'feedback.html');
+  res.sendFile(filePath);
+});
+
+app.get('/exists', (req, res) => {
+  const filePath = path.join(__dirname, 'pages', 'exists.html');
+  res.sendFile(filePath);
+});
+
+app.post('/create', async (req, res) => {
+  const title = req.body.title;
+  const content = req.body.text;
+
+  const adjTitle = title.toLowerCase();
+
+  const tempFilePath = path.join(__dirname, 'temp', adjTitle + '.txt');
+  const finalFilePath = path.join(__dirname, 'feedback', adjTitle + '.txt');
+
+  await fs.writeFile(tempFilePath, content);
+  exists(finalFilePath, async (exists) => {
+    if (exists) {
+      res.redirect('/exists');
+    } else {
+      await fs.rename(tempFilePath, finalFilePath);
+      res.redirect('/');
+    }
+  });
+});
+
+app.listen(80);
+
+
+// docker build -t feedback-node .
+// docker run -p 3000:80 -d --name feedback-app --rm feedback-node
+// localhost:3000 填写后 
+// 这样 看数据 http://localhost:3000/feedback/test.txt
